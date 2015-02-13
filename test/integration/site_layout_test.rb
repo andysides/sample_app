8d19f53bf2d6e8d7e51f2c7838d3f64c4a383474
andysides <andy.sides016@gmail.com>
2015-02-12 21:57:02 -0500

    Finish layouts, routs for Chapter 5

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  # Tests that the root_path is rendered by the correct template and that all our links are present
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
+
+end
